ae34c16d174df0af4576bea7c84d203de8fdd69d
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-14 01:17:13 +0530

    Add operate function

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,1 +1,21 @@
+import Big from 'big.js'
 
+const operate = (numberOne, numberTwo, operation) => {
+  const num1 = Big(numberOne);
+  const num2 = Big(numberTwo);
+  let result = 0;
+
+  const operations = {
+    '+': () => num1.plus(num2),
+    '-': () => num1.minus(num2),
+    'x': () => num1.times(num2),
+    '÷': () => num1.div(num2),
+    '%': () => num1.mod(num2)
+  };
+
+  result = operations[operation]();
+
+  return result;
+}
+
+export default operate
