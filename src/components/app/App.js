7d607a31b317120aad44e04c5430b928eedc456a
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-14 03:30:14 +0530

    Modify App function to class

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,17 +4,21 @@ import Display from '../display/Display';
 import ButtonPanel from '../button-panel/ButtonPanel';
 /* eslint-disable no-unused-vars */
 import calculate from '../../logic/calculate';
+import React from 'react'
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Calculator App</h1>
-      <div className={styles.app}>
-        <Display />
-        <ButtonPanel />
+export default class App extends Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        <h1 className={styles.title}>Calculator App</h1>
+        <div className={styles.app}>
+          <Display />
+          <ButtonPanel />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
