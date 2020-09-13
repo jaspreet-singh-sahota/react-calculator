a5cfc6fc07e9ba0c6514b0dea3b55df45114d967
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-14 00:29:36 +0530

    Add calculate function

---
+++ src/logic/calculate.js
@@ -0,0 +1,25 @@
+const calculate = (data, buttonName) => {
+  let { total , next , operation } = data
+  const operators = ['+', '-', '/', '*', '%']
+
+  if (buttonName === "AC") {
+    total = 0,
+    next = null,
+    operation = null
+  } else if (buttonName === "+/-") {
+    total *= -1,
+    next *= -1 
+  } else if (operators.includes(buttonName)) {
+    total = operate(total, next, operation)
+  } else if (buttonName === "=") {
+    if (total && next) {
+      total = operate(total, next, operation);
+      next = null;
+      operation = null;
+    }
+  }
+
+  return { total, next, operation }
+}
+
+export default calculate
